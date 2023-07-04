789ec17866457600516f752141673cec5a782606
sunnytower <ljxreal1@gmail.com>
2023-07-04 12:48:25 +0800

    update cs186 proj6

--- sp23-proj6/query/q1i.js
+++ sp23-proj6/query/q1i.js
@@ -1,5 +1,8 @@
 // Task 1i
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.keywords.aggregate([
+    { $match: { keywords: { $elemMatch: { $or: [{ name: "mickey mouse" }, { name: "marvel comic" } ]}}}},
+    { $sort: { movieId: 1 } },
+    { $project: { movieId: 1, _id: 0 } }
+
 ]);
--- sp23-proj6/query/q1ii.js
+++ sp23-proj6/query/q1ii.js
@@ -1,5 +1,7 @@
-// Task 1ii
-
-db.todo.aggregate([
-    // TODO: Write your query here
+db.movies_metadata.aggregate([
+    {
+        $match: { genres: { $elemMatch: { name: "Comedy" } }, vote_count: { $gte: 50 } }
+    },
+    { $sort: { vote_average: -1, vote_count: -1, movieId: 1 } },
+    { $project: { title: 1, vote_average: 1, vote_count: 1, movieId: 1, _id: 0 } }
 ]);
